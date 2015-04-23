8ef185d9d3dd0481c7d8947642e8395b3664e18d
Richard Ferrier <r.ferrier@imperial.ac.uk>
2015-04-23 13:14:59 +0100

    Fix premature binding to mode maps in init.el.

--- init-misc.el
+++ init-misc.el
@@ -1,16 +1,21 @@
 
 ;;; DIRED
 
-(define-key dired-mode-map (kbd "H-s H-8") 'dired-do-isearch-regexp)
+(eval-after-load "dired"
+    '(progn
+       (define-key dired-mode-map (kbd "H-s H-8") 'dired-do-isearch-regexp)))
 
 
 ;;; SHELL & SHELL SCRIPT
 
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt$" . shell-script-mode))
 
-(define-key shell-mode-map (kbd "H-p")
-  '(lambda () (interactive) (call-interactively 'comint-previous-input)))
-(define-key shell-mode-map (kbd "H-P")
-  '(lambda () (interactive) (call-interactively 'comint-next-input)))
+
+(eval-after-load "shell"
+  '(progn
+     (define-key shell-mode-map (kbd "H-p")
+       '(lambda () (interactive) (call-interactively 'comint-previous-input)))
+     (define-key shell-mode-map (kbd "H-P")
+       '(lambda () (interactive) (call-interactively 'comint-next-input)))))
 
 
